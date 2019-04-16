a72133f0539c01c48d7fcb9609a8990834b857c8
kael <wangyf@lbschat.com>
2019-04-16 11:26:17 +0800

    fix functions,still not stable

--- plugin.go
+++ plugin.go
@@ -1,6 +1,9 @@
 package sqflite
 
 import (
+	"database/sql"
+	"fmt"
+	"log"
 	"os"
 	"path"
 	"path/filepath"
@@ -9,13 +12,14 @@ import (
 
 	"github.com/go-flutter-desktop/go-flutter"
 	"github.com/go-flutter-desktop/go-flutter/plugin"
-	"github.com/go-xorm/xorm"
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/mitchellh/go-homedir"
 	"github.com/pkg/errors"
 )
 
 const channelName = "com.tekartik.sqflite"
+
+const errorFormat = "[SQFLITE] %v\n"
 
 const (
 	METHOD_GET_PLATFORM_VERSION = "getPlatformVersion";
@@ -74,8 +78,11 @@ type SqflitePlugin struct {
 
 	userConfigFolder string
 	codec            plugin.StandardMessageCodec
-	databases        map[int]*xorm.Engine
-	databaseId       int
+	databases        map[int]*sql.DB // store database handlers
+	databasePaths    map[int]string  // store database file path
+	databaseId       int             // store max database id
+
+	queryAsMapList bool
 }
 
 var _ flutter.Plugin = &SqflitePlugin{} // compile-time type check
@@ -111,49 +118,54 @@ func (p *SqflitePlugin) InitPlugin(messenger plugin.BinaryMessenger) error {
 	}
 
 	channel := plugin.NewMethodChannel(messenger, channelName, plugin.StandardMethodCodec{})
-	channel.HandleFunc("insert", p.handleInsert)
-	channel.HandleFunc("batch", p.handleBatch)
-	channel.HandleFunc("debugMode", p.handleDebugMode)
-	channel.HandleFunc("options", p.handleOptions)
-	channel.HandleFunc("closeDatabase", p.handleCloseDatabase)
-	channel.HandleFunc("openDatabase", p.handleOpenDatabase)
-	channel.HandleFunc("execute", p.handleExecute)
-	channel.HandleFunc("update", p.handleUpdate)
-	channel.HandleFunc("query", p.handleQuery)
-	channel.HandleFunc("getPlatformVersion", p.handleGetPlatformVersion)
-	channel.HandleFunc("getDatabasesPath", p.handleGetDatabasePath)
+	channel.HandleFunc(METHOD_INSERT, p.handleInsert)
+	channel.HandleFunc(METHOD_BATCH, p.handleBatch)
+	channel.HandleFunc(METHOD_DEBUG_MODE, p.handleDebugMode)
+	channel.HandleFunc(METHOD_OPTIONS, p.handleOptions)
+	channel.HandleFunc(METHOD_CLOSE_DATABASE, p.handleCloseDatabase)
+	channel.HandleFunc(METHOD_OPEN_DATABASE, p.handleOpenDatabase)
+	channel.HandleFunc(METHOD_EXECUTE, p.handleExecute)
+	channel.HandleFunc(METHOD_UPDATE, p.handleUpdate)
+	channel.HandleFunc(METHOD_QUERY, p.handleQuery)
+	channel.HandleFunc(METHOD_GET_PLATFORM_VERSION, p.handleGetPlatformVersion)
+	channel.HandleFunc(METHOD_GET_DATABASES_PATH, p.handleGetDatabasePath)
+	channel.HandleFunc("deleteDatabase", p.handleDeleteDatabase)
 	channel.HandleFunc("databaseExists", p.handleDatabaseExists)
-	//channel.HandleFunc("deleteDatabase", p.handleDeleteDatabase)
 	return nil
 }
 
-func (p *SqflitePlugin) handleInsert(arguments interface{}) (reply interface{}, err error) {
-	cacheDir, err := os.UserCacheDir()
-	if err != nil {
-		return nil, err
-	}
-	return filepath.Join(cacheDir, p.VendorName, p.ApplicationName), nil
+func (p *SqflitePlugin) handleGetPlatformVersion(arguments interface{}) (reply interface{}, err error) {
+	version := fmt.Sprintf("%s %s", runtime.GOOS, runtime.GOARCH)
+	return version, nil
 }
 
-func (p *SqflitePlugin) handleBatch(arguments interface{}) (reply interface{}, err error) {
-	return filepath.Join(p.userConfigFolder, p.VendorName, p.ApplicationName), nil
-}
-
-func (p *SqflitePlugin) handleDebugMode(arguments interface{}) (reply interface{}, err error) {
-	return filepath.Join(p.userConfigFolder, p.VendorName, p.ApplicationName), nil
+func (p *SqflitePlugin) handleGetDatabasePath(arguments interface{}) (reply interface{}, err error) {
+	return p.userConfigFolder, nil
 }
 
 func (p *SqflitePlugin) handleOptions(arguments interface{}) (reply interface{}, err error) {
+	var args map[string]interface{}
+	var ok bool
+	if args,ok=arguments.(map[string]interface{});!ok {
+		return nil, errors.New("invalid param for option call")
+	}
+	paramAsList,ok := args["PARAM_QUERY_AS_MAP_LIST"]
+	if ok {
+		p.queryAsMapList = paramAsList.(bool)
+	}
 	return filepath.Join(p.userConfigFolder, p.VendorName, p.ApplicationName), nil
 }
 
 func (p *SqflitePlugin) handleCloseDatabase(arguments interface{}) (reply interface{}, err error) {
-	var db *xorm.Engine
-	db, err = p.getDatabase(arguments)
+	databaseId, db, err := p.getDatabase(arguments)
 	if err != nil {
 		return nil, err
 	}
 	err = db.Close()
+	p.Lock()
+	defer p.Unlock()
+	delete(p.databasePaths, databaseId)
+	delete(p.databases, databaseId)
 	return nil, err
 }
 
@@ -164,58 +176,257 @@ func (p *SqflitePlugin) handleOpenDatabase(arguments interface{}) (reply interfa
 		return nil, errors.New("invalid arguments")
 	}
 	var dbpath string
+	var readOnly bool
+	var singleInstance bool
 	if dpath, ok := args[PARAM_PATH]; ok {
 		dbpath = dpath.(string)
 	}
+	if rdo,ok:=args[PARAM_READ_ONLY];ok {
+		readOnly = rdo.(bool)
+	}
+	if si,ok:=args[PARAM_SINGLE_INSTANCE];ok{
+		singleInstance = si.(bool) && MEMORY_DATABASE_PATH!=dbpath
+	}
 	if dbpath == "" {
+		log.Printf(errorFormat, "invalid dbpath")
 		return nil, errors.New("invalid dbpath")
 	}
-	var engine *xorm.Engine
-	engine, err = xorm.NewEngine("sqlite3", path.Join(p.userConfigFolder, dbpath))
+	if readOnly{
+		log.Printf(errorFormat, "readonly not supported")
+	}
+	if MEMORY_DATABASE_PATH!=dbpath {
+		err = os.MkdirAll(path.Dir(dbpath), 0755)
+		if err != nil {
+			log.Printf(errorFormat, err.Error())
+		}
+	}
+	if singleInstance {
+		dbId,ok:=p.getDatabaseByPath(dbpath)
+		if ok {
+			return map[string]interface{}{
+				PARAM_ID: dbId,
+				PARAM_RECOVERED: true,
+			}, nil
+		}
+	}
+	var engine *sql.DB
+	engine, err = sql.Open("sqlite3", dbpath)
 	if err != nil {
 		return nil, err
 	}
 	p.Lock()
+	defer p.Unlock()
 	p.databaseId++
 	p.databases[p.databaseId] = engine
-	p.Unlock()
-	return p.databaseId, nil
+	p.databasePaths[p.databaseId] = dbpath
+	return map[string]interface{}{
+		PARAM_ID: p.databaseId,
+		PARAM_RECOVERED: false,
+	}, nil
+}
+
+func (p *SqflitePlugin) handleInsert(arguments interface{}) (reply interface{}, err error) {
+	_, db,err:=p.getDatabase(arguments)
+	if err!=nil{
+		return 0,err
+	}
+	sqlStr,args,err := p.getSqlCommand(arguments)
+	if err!=nil{
+		return nil,err
+	}
+	result,err:=db.Exec(sqlStr, args)
+	if err!=nil{
+		return 0, err
+	}
+	return result.LastInsertId(), nil
+}
+
+func (p *SqflitePlugin) handleBatch(arguments interface{}) (reply interface{}, err error) {
+	_,db,err:=p.getDatabase(arguments)
+	if err!=nil{
+		return nil,err
+	}
+	args,ok:=arguments.(map[string]interface{})
+	if !ok {
+		return nil, errors.New("invalid args")
+	}
+	ioperations,ok:=args[PARAM_OPERATIONS]
+	if !ok{
+		return nil, errors.New("invalid operation")
+	}
+	operations, ok:=ioperations.([]map[string]interface{})
+	if !ok {
+		return  nil, errors.New("invalid operation data format")
+	}
+	for _,operate:=range operations {
+		mtd,ok:=operate[PARAM_METHOD]
+		if !ok {
+			return nil,errors.New("empty method")
+		}
+		method,ok := mtd.(string)
+		if !ok {
+			return  nil, errors.New("invalid method")
+		}
+		sqlStr,xargs,err:=p.getSqlCommand(operate)
+		if err!=nil{
+			return nil,err
+		}
+		switch method {
+		case METHOD_UPDATE:
+			fallthrough
+		case METHOD_INSERT:
+			fallthrough
+		case METHOD_EXECUTE:
+			_,err=db.Exec(sqlStr, xargs)
+			if err!=nil{
+				return nil,err
+			}
+		case METHOD_QUERY:
+			_,err=db.Query(sqlStr, xargs)
+			if err!=nil{
+				return nil,err
+			}
+		default:
+			return nil, errors.New("Invalid batch param")
+		}
+	}
+	return nil, nil
+}
+
+func (p *SqflitePlugin) handleDebugMode(arguments interface{}) (reply interface{}, err error) {
+	// do nothing now
+	return nil, nil
 }
 
 func (p *SqflitePlugin) handleExecute(arguments interface{}) (reply interface{}, err error) {
-	return filepath.Join(p.userConfigFolder, p.VendorName, p.ApplicationName), nil
+	_, db,err:=p.getDatabase(arguments)
+	if err!=nil{
+		return nil,err
+	}
+	sqlStr,args,err := p.getSqlCommand(arguments)
+	if err!=nil{
+		return nil,err
+	}
+	_,err=db.Exec(sqlStr, args)
+	if err!=nil{
+		return nil, err
+	}
+	return nil, nil
 }
 
 func (p *SqflitePlugin) handleUpdate(arguments interface{}) (reply interface{}, err error) {
-	return filepath.Join(p.userConfigFolder, p.VendorName, p.ApplicationName), nil
+	_, db,err:=p.getDatabase(arguments)
+	if err!=nil{
+		return 0,err
+	}
+	sqlStr,args,err := p.getSqlCommand(arguments)
+	if err!=nil{
+		return nil,err
+	}
+	result,err:=db.Exec(sqlStr, args)
+	if err!=nil{
+		return 0, err
+	}
+	return result.RowsAffected(), nil
 }
 
 func (p *SqflitePlugin) handleQuery(arguments interface{}) (reply interface{}, err error) {
-	return filepath.Join(p.userConfigFolder, p.VendorName, p.ApplicationName), nil
-}
-
-func (p *SqflitePlugin) handleGetPlatformVersion(arguments interface{}) (reply interface{}, err error) {
-	return filepath.Join(p.userConfigFolder, p.VendorName, p.ApplicationName), nil
-}
-
-func (p *SqflitePlugin) handleGetDatabasePath(arguments interface{}) (reply interface{}, err error) {
-	return filepath.Join(p.userConfigFolder, p.VendorName, p.ApplicationName), nil
+	_, db,err:=p.getDatabase(arguments)
+	if err!=nil{
+		return nil,err
+	}
+	sqlStr,args,err := p.getSqlCommand(arguments)
+	if err!=nil{
+		return nil,err
+	}
+	rows,err:=db.Query(sqlStr, args)
+	if err!=nil{
+		return nil, err
+	}
+	cols,err:=rows.Columns()
+	if err!=nil{
+		return nil, err
+	}
+	var resultRows []map[string]interface{}
+	for {
+		resultRow := make(map[string]interface{})
+		dest:=make([]interface{}, len(cols))
+		err = rows.Scan(dest...)
+		for idx,col:=range cols {
+			resultRow[col] = dest[idx]
+		}
+		resultRows = append(resultRows, resultRow)
+		if !rows.Next() {
+			break
+		}
+	}
+	return resultRows, nil
 }
 
 func (p *SqflitePlugin) handleDatabaseExists(arguments interface{}) (reply interface{}, err error) {
-	return filepath.Join(p.userConfigFolder, p.VendorName, p.ApplicationName), nil
+	return false, nil
 }
 
-//func (p *SqflitePlugin) handleDeleteDatabase(arguments interface{}) (reply interface{}, err error) {
-//	return filepath.Join(p.userConfigFolder, p.VendorName, p.ApplicationName), nil
-//}
-
-func (p *SqflitePlugin) getDatabase(arguments interface{}) (*xorm.Engine, error) {
-	switch arguments.(type) {
-	case int:
-		if db, ok := p.databases[arguments.(int)]; ok {
-			return db, nil
+func (p *SqflitePlugin) handleDeleteDatabase(arguments interface{}) (reply interface{}, err error) {
+	if dbPath,ok:=arguments.(string);ok {
+		if dbPath!=MEMORY_DATABASE_PATH {
+			err = os.Remove(dbPath)
 		}
 	}
-	return nil, errors.New("invalid database")
+	return nil, err
+}
+
+func (p *SqflitePlugin) getDatabase(arguments interface{}) (int, *sql.DB, error) {
+	var args map[string]interface{}
+	var ok bool
+	if args,ok=arguments.(map[string]interface{});!ok {
+		return -1, nil, errors.New("db not found")
+	}
+	if dbId,ok:=args[PARAM_ID];ok{
+		p.Lock()
+		defer p.Unlock()
+		id,ok:=dbId.(int)
+		if !ok {
+			return -1, nil, errors.New("Invaid db id")
+		}
+		if db,ok:=p.databases[id];ok {
+			return id, db, nil
+		}
+	}
+	return -1, nil, errors.New("invalid database")
+}
+
+func (p *SqflitePlugin) getDatabaseByPath(dbPath string) (int, bool) {
+	if dbPath==MEMORY_DATABASE_PATH{
+		return -1, false
+	}
+	p.Lock()
+	defer p.Unlock()
+	for id,pt:= range p.databasePaths {
+		if pt==dbPath {
+			return id, true
+		}
+	}
+	return -1, false
+}
+
+func (p *SqflitePlugin) getSqlCommand(arguments interface{}) (sqlStr string, xargs []interface{}, err error) {
+	var args map[string]interface{}
+	var ok bool
+	if args,ok=arguments.(map[string]interface{});!ok {
+		return "", nil, errors.New("db not found")
+	}
+	tsql,ok:=args[PARAM_SQL]
+	if !ok {
+		return "",nil, errors.New("SQL is not set")
+	}
+	sqlStr = tsql.(string)
+	if sqlStr=="" {
+		return "", nil, errors.New("SQL is empty")
+	}
+	targs,ok:=args[PARAM_SQL_ARGUMENTS]
+	if ok {
+		xargs,_=targs.([]interface{})
+	}
+	return
 }
